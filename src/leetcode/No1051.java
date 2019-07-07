8da80f381962780d0ff871805fa7bf423690dd93
siye.dsy <siye.dsy@alibaba-inc.com>
2019-07-07 20:36:08 +0800

    1051 solved

---
+++ src/leetcode/No1051.java
@@ -0,0 +1,15 @@
+package leetcode;
+
+import java.util.Arrays;
+
+public class No1051 {
+    public int heightChecker(int[] heights) {
+        int[] copy = Arrays.copyOf(heights, heights.length);
+        int count = 0;
+        Arrays.sort(heights);
+        for (int i = 0 ; i < heights.length ; i++){
+            if (copy[i] != heights[i]) count++;
+        }
+        return count;
+    }
+}
