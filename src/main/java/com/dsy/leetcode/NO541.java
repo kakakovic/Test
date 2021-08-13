f58cea4d666328030a626edab699764cd85ad3b1
丁斯也 <dingsiye@gmail.com>
2021-08-14 00:19:23 +0800

    up

--- src/main/java/com/dsy/leetcode/NO541.java
+++ src/main/java/com/dsy/leetcode/NO541.java
@@ -1,28 +1,44 @@
-//package com.dsy.leetcode;
-//
-//import java.util.ArrayList;
-//import java.util.List;
-//
-//public class NO541 {
-//    public String reverseStr(String s, int k) {
-//        List<String> result = new ArrayList<>();
-//        int slots = s.length() / (2 * k);
-//        for (int i = 0 ; i < slots ; i++){
-//            String x = s.substring(2 * k * i, 2 * k * i + 2 * k);
-//            result.add(x);
-//        }
-//        for (String x : result){
-//
-//        }
-//    }
-//
-//    public String reverse(String s){
-//        char[] tmp = s.toCharArray();
-//        for (int i = 0 ; i < (tmp.length/2) ; i++){
-//            char x = tmp[i];
-//            tmp[i] = tmp[tmp.length - i];
-//            tmp[tmp.length - i] = x;
-//        }
-//        return new String(tmp);
-//    }
-//}
+package com.dsy.leetcode;
+
+import java.util.*;
+
+public class NO541 {
+    public String reverseStr(String s, int k) {
+        StringBuilder stringBuilder = new StringBuilder();
+        Queue<Character> queue = new LinkedList<>();
+        int length = 2 * k;
+        int count = 0;
+        for (int i = 0 ; i < s.length() ; i++){
+            queue.add(s.charAt(i));
+            count++;
+            if (count % length == 0){
+                Stack<Character> tmp = new Stack<>();
+                for (int j = 1 ; j <= k ; j++){
+                    tmp.add(queue.poll());
+                }
+                while (!tmp.empty()){
+                    stringBuilder.append(tmp.pop());
+                }
+                while (!queue.isEmpty()){
+                    stringBuilder.append(queue.poll());
+                }
+            }
+        }
+        if (queue.isEmpty()){
+            return stringBuilder.toString();
+        }else {
+            int size = queue.size();
+            Stack<Character> tmp = new Stack<>();
+            for (int j = 1 ; j <= k && j <= size ; j++){
+                tmp.add(queue.poll());
+            }
+            while (!tmp.empty()){
+                stringBuilder.append(tmp.pop());
+            }
+            while (!queue.isEmpty()){
+                stringBuilder.append(queue.poll());
+            }
+            return stringBuilder.toString();
+        }
+    }
+}
--- src/main/java/com/dsy/leetcode/NO547.java
+++ src/main/java/com/dsy/leetcode/NO547.java
@@ -6,13 +6,36 @@ import java.util.List;
 public class NO547 {
     public static void main(String[] args) {
         NO547 no547 = new NO547();
-        System.out.println(no547.findCircleNum(new int[][]{new int[]{1,0,0,1}, new int[]{0,1,1,0}, new int[]{0,1,1,1}, new int[]{1,0,1,1}}));
+
+        System.out.println(no547.findCircleNum(new int[][]{
+                new int[]{1,1,0,0,0,0,0,1,0,0,0,0,0,0,0},
+                new int[]{1,1,0,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,1,0,0,0,0,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,1,0,1,1,0,0,0,0,0,0,0,0},
+                new int[]{0,0,0,0,1,0,0,0,0,1,1,0,0,0,0},
+
+                new int[]{0,0,0,1,0,1,0,0,0,0,1,0,0,0,0},
+                new int[]{0,0,0,1,0,0,1,0,1,0,0,0,0,1,0},
+                new int[]{1,0,0,0,0,0,0,1,1,0,0,0,0,0,0},
+                new int[]{0,0,0,0,0,0,1,1,1,0,0,0,0,1,0},
+                new int[]{0,0,0,0,1,0,0,0,0,1,0,1,0,0,1},
+
+                new int[]{0,0,0,0,1,1,0,0,0,0,1,1,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,1,1,1,0,0,0},
+                new int[]{0,0,0,0,0,0,0,0,0,0,0,0,1,0,0},
+                new int[]{0,0,0,0,0,0,1,0,1,0,0,0,0,1,0},
+                new int[]{0,0,0,0,0,0,0,0,0,1,0,0,0,0,1},
+        }));
     }
 
     public int findCircleNum(int[][] isConnected) {
         int n = isConnected.length;
+        //并查集元素集合
         List<List<Integer>> linkList = new ArrayList<>();
+
+        //当前元素属于哪个并查集
         List<Integer> refer = new ArrayList<>();
+
         //所有城市都和自己相连
         for (int i = 0 ; i < n ; i++){
             List<Integer> data = new ArrayList<>();
@@ -26,24 +49,30 @@ public class NO547 {
                 if (isConnected[i][j] == 1){
                     int referIndexLeft = refer.get(i);
                     int referIndexRight = refer.get(j);
+                    //将靠后并查集中的元素尽可能挪到前面的并查集中
                     if (referIndexLeft == referIndexRight){
                         continue;
-                    }else if (referIndexLeft < referIndexRight){
-                        int referIndex = refer.get(j);
+                    }else{
+                        //要挪动的并查集
+                        int referIndex;
+                        //要挪动到的并查集
+                        int toIndex;
+                        if (referIndexLeft < referIndexRight){
+                            referIndex = refer.get(j);
+                            toIndex = refer.get(i);
+                        }else {
+                            referIndex = refer.get(i);
+                            toIndex = refer.get(j);
+
+                        }
+
                         List<Integer> referData = linkList.get(referIndex);
-                        int toIndex = refer.get(i);
-                        linkList.get(i).addAll(referData);
-                        linkList.set(referIndex, new ArrayList<>());
-                        refer.set(j, toIndex);
-                    }else {
-                        int referIndex = refer.get(i);
-                        List<Integer> referData = linkList.get(referIndex);
-                        int toIndex = refer.get(j);
                         linkList.get(toIndex).addAll(referData);
                         linkList.set(referIndex, new ArrayList<>());
-                        refer.set(i, toIndex);
+                        for (Integer num : referData){
+                            refer.set(num, toIndex);
+                        }
                     }
-
                 }
             }
         }
